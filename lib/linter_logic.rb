70411560ff519b7b2db94241562bc92d59e5ce52
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-08 13:34:26 -0600

    create first linter checker

--- lib/linter_logic.rb
+++ lib/linter_logic.rb
@@ -1,9 +1,39 @@
-def get_string
-  File.open('lib/test.md', 'r+') do |file|
-    str = ''
-    while line = file.gets
-      str.concat(line)
+require 'strscan'
+
+class Checker
+  attr_reader :buffer
+  @@line_count = 0
+
+  def initialize(str)
+    @buffer = StringScanner.new(str)
+    @@line_count += 1
+    scan
+  end
+
+  def scan
+    until @buffer.eos?
+      error_checker
     end
-    str
+  end
+
+  def error_checker
+    num_sign_check
+  end
+
+  def num_sign_check
+    if buffer.scan_until(/#[^#+\s]/)
+      puts "Warning in Line #{@@line_count}, position: #{buffer.pos}: '#{buffer.matched}'. Always put a space between the number signs '#' and the heading name"
+      buffer.terminate
+    else
+      buffer.terminate
+    end
+  end
+end
+
+def start_linter
+  File.open('lib/test.md', 'r+') do |file|
+    while line = file.gets
+      Checker.new(line)
+    end
   end
 end
