aa10b6ecc7fc97adb2f66669175a314942e21134
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-09 13:22:07 -0600

    add one test for Checker and #initialize

--- spec/linter_logic_spec.rb
+++ spec/linter_logic_spec.rb
@@ -1,3 +1,15 @@
 require_relative '../lib/linter_logic'
 
+describe Checker do
 
+  describe '#initialize' do
+    subject(:create_string_scanner) { described_class.new('this is a string', 1) }
+
+    it 'creates a new StringScanner object' do
+      string_scanner = create_string_scanner.buffer
+      expect(string_scanner).to be_instance_of(StringScanner)
+    end
+
+  end
+
+end
