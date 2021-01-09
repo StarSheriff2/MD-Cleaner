d0f5cc83f7083afa0eb8c9195827e7dc010f0f5f
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-08 22:35:13 -0600

    fix linter errors

--- lib/linter_logic.rb
+++ lib/linter_logic.rb
@@ -2,45 +2,50 @@ require 'strscan'
 
 class Checker
   attr_reader :buffer
-  @@line_count = 0
 
-  def initialize(str)
+  def initialize(str, line_number)
     @buffer = StringScanner.new(str)
-    @@line_count += 1
+    @line_number = line_number
     @checkers = [heading, paragraph_indent, italic_middle]
     error_checker
   end
 
   def error_checker
     @checkers.each do |checker|
-      checker
+      puts checker if checker
       @buffer.reset
     end
   end
 
   def heading
-    if buffer.scan_until(/#[^#+\s]/)
-      puts "Warning in Line #{@@line_count}, position: #{buffer.pos}: '#{buffer.matched}'. => Always put a space between the number signs, '#', and the heading name."
-    end
+    return unless buffer.scan_until(/#[^#+\s]/)
+
+    "Warning in Line #{@line_number}, position #{buffer.pos}: '#{buffer.matched}'. \
+    => Always put a space between the number signs, '#', and the heading name."
   end
 
   def paragraph_indent
-    if buffer.scan_until(/^\s+\S+/)
-      puts "Warning in Line #{@@line_count}, position: #{buffer.pos}: '#{buffer.matched}'. => Unless the paragraph is in a list, don’t indent paragraphs with spaces or tabs."
-    end
+    return unless buffer.scan_until(/^\s+\S+/)
+
+    "Warning in Line #{@line_number}, position #{buffer.pos}: '#{buffer.matched}'. \
+    => Unless the paragraph is in a list, don’t indent paragraphs with spaces or tabs."
   end
 
   def italic_middle
-    if buffer.scan_until(/\w+_\w+_\w+/)
-      puts "Warning in Line #{@@line_count}, position: #{buffer.pos}: '#{buffer.matched}'. => Use asterisks to italicize the middle of a word for emphasis."
-    end
+    return unless buffer.scan_until(/\w+_\w+_\w+/)
+
+    "Warning in Line #{@line_number}, position #{buffer.pos}: '#{buffer.matched}'. \
+    => Use asterisks to italicize the middle of a word for emphasis."
   end
 end
 
-def start_linter(file = 'lib/test.md')
-  File.open(file, 'r+') do |file|
-    while line = file.gets
-      Checker.new(line)
+def start_linter(file)
+  line_count = 0
+
+  File.open(file, 'r+') do |f|
+    while (line = f.gets)
+      line_count += 1
+      Checker.new(line, line_count)
     end
   end
 end
