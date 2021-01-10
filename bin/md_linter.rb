5c226d4935f19101d794d5d60e50e51cf71786ba
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-01-09 21:56:19 -0600

    edit text in md_linter.rb

--- bin/md_linter.rb
+++ bin/md_linter.rb
@@ -4,12 +4,12 @@ require_relative '../lib/linter_logic'
 require_relative '../lib/error_checkers'
 
 # Select the file you want to test here. By default the file is lib/test.md.
-# Change the value of file to the directory where your md file is
+# Copy or move your markdown file into the lib/ sub-directory of this program's root directory
 
 file = 'lib/test.md'
 line_count = 0
 
-def error_checker(check, checkers)
+def run_linter(check, checkers)
   i = 0
   while i < checkers.length do
     if check.match_check(checkers[i].pattern)
@@ -25,6 +25,6 @@ File.open(file, 'r+') do |f|
   while (line = f.gets)
     line_count += 1
     check = Check.new(line, line_count)
-    error_checker(check, check.checkers, )
+    run_linter(check, check.checkers)
   end
 end
